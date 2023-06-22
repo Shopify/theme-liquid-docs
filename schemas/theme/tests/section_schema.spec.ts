627d01b34a7b8b236963eb2b900d702b70e3c825
Albert Chu <albert.chu@shopify.com>
2023-06-22 23:01:12 +0000

    vitest setup

---
+++ schemas/theme/tests/section_schema.spec.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest';
+
+
+describe('JSON Schema validation for Theme Liquid Section Schemas', () => {
+
+    it('should work', () => {
+        expect(1).toBe(1);
+    });
+ })
